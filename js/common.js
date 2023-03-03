1b9ea266371a49eca8286e76e389bddfbaf63682
threehHong <honghyohun@naver.com>
2023-03-03 18:22:52 +0900

    esc

---
+++ js/common.js
@@ -0,0 +1,13 @@
+let asideMenu = $('.aside_menu');
+let title = asideMenu.find('a');
+
+/* title.each(function(idx, item){
+    item.click(function(){
+        titleName = $(this).val();
+        console.log(titleName)
+    })
+}) */
+title.click(function(){
+    /* titleName = $(this).val();
+    console.log(titleName) */
+})
